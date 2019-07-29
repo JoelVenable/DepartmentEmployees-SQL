f382f8a093136c3cdd5d5006d2b9e5b0573d014c
JoelVenable <30368779+JoelVenable@users.noreply.github.com>
2019-07-29 16:21:55 -0500

    Finished exercise

---
+++ DepartmentsEmployees/Models/Department.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace DepartmentsEmployees.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string DeptName { get; set; }
+    }
+
+
+}
